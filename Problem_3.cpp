e1679f737d4f389dd19f90014521195d2d5fb663
Imtiaj Sajin <100506477+Imtiaj-Sajin@users.noreply.github.com>
2022-09-27 22:57:25 +0600

    Add files via upload
    Initialize an array. Size should be more than FIVE. Write you program to change the array in such a way so that there cannot be any duplicate element in the array anymore. Print the changed array. If the initialized array already had no duplicate elements from the beginning, output a message saying “Array already unique!”;

---
+++ Problem_3.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int array[10];
+int array2[10];
+int i,j,k=0;
+
+ for (int i = 0; i < 10; i++)
+    {
+        cin>>array[i];
+    }
+    
+for(i=0;i<10;i++)
+{
+    for(j=i+1;j<10;j++){
+        if(array[i]==array[j]){
+            break;
+        }
+    }
+    if(j==10){
+        array2[k]=array[i];
+        k++;
+    }
+}
+    if(k==10){
+        cout<<"Already unique element";
+    }
+    else
+    {
+        for(i=0;i<k;i++)
+            cout<<array2[i]<<" ";
+    }
+return 0;
+}
