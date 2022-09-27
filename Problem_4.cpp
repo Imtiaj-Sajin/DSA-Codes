9a288bd4924c71b2e33519f5b82e9d057ce6bb18
Imtiaj Sajin <100506477+Imtiaj-Sajin@users.noreply.github.com>
2022-09-27 13:36:59 +0600

    Problem_4 added
    Initialize an integer array A of size 10. Take an integer as input and print how many times that integer occurs in A.
    
    For example,
    Array_1 = {8,4,6,1,6,9,6,1,9,8}
    
    Output:
    Input a number to search: 6
    The number occurs 3 times in the array

---
+++ Problem_4.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int count=0,num;
+    int Array_1[10] = {8,4,6,1,6,9,6,1,9,8};
+    cout<<"Enter a number to search: ";
+    cin>>num;
+    for(int i=0;i<10;i++)
+    {
+        if(Array_1[i]==num)
+        {
+            count++;
+        }
+    }
+
+    if(count!=0)
+    {
+        cout<<"The number occurs "<<count<<" times in the array."<<endl;
+
+    }
+    else
+        cout<<"Number not found.";
+}
+
+
