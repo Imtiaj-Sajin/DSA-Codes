eea23e38b8c4c3b0af7fbdb1b740d1e18639d3a6
Imtiaj Sajin <100506477+Imtiaj-Sajin@users.noreply.github.com>
2022-09-27 13:37:53 +0600

    Problem_5 added
    Initialize an integer array of size 10. Print the number of time each element occurs in the array.
    
    For example,
    Array_1 = {8,4,6,1,6,9,6,1,9,8}
    
    Output:
    8 occurs = 2 times
    4 occurs = 1 time
    6 occurs = 3 times
    1 occurs = 2 times
    9 occurs = 2 times

---
+++ Problem_5.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int count=0,flag=0;
+    int Array_1[10] = {8,4,6,1,6,9,6,1,9,8};
+    cout<<"Enter a number to search: ";
+
+    for(int i=0;i<10;i++)
+    {
+      if(flag==0)
+        {
+
+        for(int j=0;j<10;j++)
+        {
+          if(Array_1[i]==Array_1[j])
+            {
+            count++;
+            }
+        }
+        cout<<Array_1[i]<<" occurs "<<count<<" times"<<endl;
+
+        count==0;
+        }
+
+    }
+
+}
+
