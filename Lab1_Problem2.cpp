e1c3c31dd59e26ed68b6d4047acd98266f0bd81f
Imtiaj Sajin <100506477+Imtiaj-Sajin@users.noreply.github.com>
2022-09-27 20:45:48 +0600

    Add files via upload

---
+++ Lab1_Problem2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int arr[10]={1,2,3,4,5,6,7,8,9,10};
+    cout<<"All values in array: ";
+
+    for(int i=0; i<10;i++){
+
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
