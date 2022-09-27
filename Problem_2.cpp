26fee3e5b8442e6a869be5a59a15b0f0e15d907c
Imtiaj Sajin <100506477+Imtiaj-Sajin@users.noreply.github.com>
2022-09-27 13:18:22 +0600

    Problem_2 added
    Initialize TWO integer arrays A and B of different sizes. Make a new array with the common elements between A and B. Print the new array element(s). If there is no common element, output “No common element!”.
    
    For example,
    Scenario 1:
    Array_1 = {1,4,6,3,6,9}
    Array_2 = {5,3,7,1,2,6}
    
    Output: 1 6 3
    
    Scenario 2:
    Array_1 = {1,4,6,3,6,9}
    Array_2 = {5,8,7,12,21,63}
    
    Output: No common element!

---
+++ Problem_2.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=0;
+    int arr1 [6];
+    int arr2 [5];
+    int arr3[12];
+    cout<<"Input array 1: "<<endl;
+      for(int j=0;j<6;j++)
+    {
+
+       cin>>arr1[j];
+    }
+    cout<<"Input array 2: "<<endl;
+
+    for(int j=0;j<5;j++)
+    {
+
+       cin>>arr2[j];
+    }
+    for(int i=0; i<6;i++){
+        for(int j=0; j<6;j++){
+            if(arr1[i]==arr2[j]){
+                arr3[x]=arr1[i];
+            x++;
+            }
+        }
+    }
+    if(x!=0)
+    for(int i=0; i<x;i++)
+        cout<<"Common Elements"<<arr3[i]<<",";
+    else
+        cout<<"No common element!";
+
+
+}
