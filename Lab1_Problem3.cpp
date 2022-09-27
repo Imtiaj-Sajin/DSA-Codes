efe4bd8a48eaa7cbe42f7da374f31ffffed9dddb
Imtiaj Sajin <100506477+Imtiaj-Sajin@users.noreply.github.com>
2022-09-27 20:47:18 +0600

    Add files via upload

---
+++ Lab1_Problem3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[10
+    cout<<"Take Input : ";
+    for(int i=0; i<10;i++){
+        
+        cin>>arr[i];
+    }
+
+
+    for(int i=0; i<10; i++){
+        cout<<arr[i]<<" ";
+    }
+}
