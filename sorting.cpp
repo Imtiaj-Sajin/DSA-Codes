10f731be5158287400445f100b5b64fe8641c27a
Imtiaj Sajin <100506477+Imtiaj-Sajin@users.noreply.github.com>
2022-10-13 12:21:09 +0600

    Update sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -4,14 +4,22 @@ using namespace std;
 
 int main()
 {
-    int arr[6] = {55,33,66,77,22,11};
+    int arr[6];
     int min_index =0;
     int temp=0;
+
+    cout<<"Input integer number:"<<endl;
+    for(int i=0; i<6;i++){
+        cin>>arr[i];
+    }
+    cout<<"Your entered values: "<<endl;
 
     for(int i=0; i<6;i++){
         cout<<arr[i]<<" ";
     }
+
     cout<<endl<<"---------"<<endl;
+    cout<<"Your sorted values: "<<endl;
 
     for(int i=0; i<6; i++){
         min_index = i;
