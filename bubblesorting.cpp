4d532e563fbf52107339047b3a0237f16cc991aa
Imtiaj Sajin <100506477+Imtiaj-Sajin@users.noreply.github.com>
2022-10-18 12:07:27 +0600

    Create bubblesorting.cpp

---
+++ bubblesorting.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+    int temp;
+
+    cout<<"Total number you wanna input:";
+    cin>>n;
+
+    int arr[n];
+
+    cout<<"Input "<<n<<" integer number:"<<endl;
+    for(int i=0; i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"Your entered values: "<<endl;
+
+    for(int i=0; i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    cout<<endl<<"---------"<<endl;
+    cout<<"Your sorted values: "<<endl;
+
+    for(int i=0; i<(n-1); i++)
+    {
+         for(int j=0; j<(n-i-1); j++)
+        {
+            if(arr[j] > arr[j+1])
+            {
+               temp = arr[j];
+               arr[j] = arr[j+i];
+               arr[j+i] = temp;
+
+            }
+        }
+
+
+    }
+
+    for(int i=0; i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    cout<<endl;
+
+    return 0;
+}
