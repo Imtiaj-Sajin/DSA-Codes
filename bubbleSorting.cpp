be1246db7a9536d6c5db70ee17dbb8176e4829a5
Imtiaj Sajin <100506477+Imtiaj-Sajin@users.noreply.github.com>
2022-10-20 00:02:55 +0600

    Update bubbleSorting.cpp

--- bubbleSorting.cpp
+++ bubbleSorting.cpp
@@ -26,9 +26,9 @@ int main()
     cout<<endl<<"---------"<<endl;
     cout<<"Your sorted values: "<<endl;
 
-    for(int i=0; i<(n-1); i++)
+    for(int i=0; i<(n); i++)
     {
-         for(int j=0; j<(n-i-1); j++)
+         for(int j=0; j<(n-1); j++)
         {
             if(arr[j] > arr[j+1])
             {
