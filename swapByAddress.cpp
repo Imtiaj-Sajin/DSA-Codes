02d670d1ce4c06c01c298fb2570783addb8a052d
Imtiaj Sajin <100506477+Imtiaj-Sajin@users.noreply.github.com>
2022-10-31 08:34:06 +0600

    Update swapByAddress.cpp

--- swapByAddress.cpp
+++ swapByAddress.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-void Wap(int *x,int *y)
+void Swap(int *x,int *y)
 {
     int t;
     t=*x;
@@ -15,22 +15,13 @@ int main(){
     int A[]={9,4};
      i=8;
     int b=4;
-  cout<<&b;
+  
+    cout<<"Before Swapping:"<<endl;
+    cout<<a<<"   "<<b<<endl;
+    
+    Swap(&i,&b);
 
-    Wap(&i,&b);
-
-    cout<<b;
-    // lenth=sizeof(A)/4;
-    // for(int j=1; j<lenth;j++){
-    //     value=A[j];
-    //     i=j-1;
-    //     while(i>=0&&A[i]>value){
-    //         A[i+1]=A[i];
-    //         i=i-1;
-    //     }
-    //     A[i+1]=value;
-    // }
-    // for(i=0;i<lenth;i++){
-    //     cout<<A[i]<<" ";
-    // }
+    cout<<"After Swapping:"<<endl;
+    cout<<a<<"   "<<b<<endl;
+    
 }
