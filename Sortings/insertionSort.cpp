bd8004b92995f437f394ce5c423f481e76b48d39
Imtiaj Sajin <100506477+Imtiaj-Sajin@users.noreply.github.com>
2023-02-21 15:52:47 +0600

    Create insertionSort.cpp

---
+++ Sortings/insertionSort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main (){
+
+int i,j,num=6,temp,arr [6]={3,2,33,22,11,76};
+
+
+for(i=1;i<num;i++){
+
+    temp = arr[i];//i = starting index
+    j =i-1;
+    while ((temp<arr [j])&& (j>=0)){ // j=current index,temp = eoh
+
+        arr[j+1]=arr[j];
+        j=j-1;
+    }
+    arr[j+1]=temp;
+}
+cout<<"sorted elements " <<endl;
+for (i =0;i<num;i++)
+{
+    cout<< arr [i]<<endl;
+}
+return 0;
+}
