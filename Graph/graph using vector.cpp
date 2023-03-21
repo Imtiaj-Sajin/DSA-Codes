5c127a1ddd69d5dc0525a0c19a8b4a034af68608
Imtiaj Sajin <100506477+Imtiaj-Sajin@users.noreply.github.com>
2023-03-21 10:19:32 +0600

    Create graph using vector.cpp

---
+++ Graph/graph using vector.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+vector<int> g[100];
+int node,edge;
+cin>>node>>edge;
+
+    int u,v;
+    for(int i=0;i<edge;i++)
+    {
+        cin>>u>>v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+for(int i=0;i<node;j++)
+{
+    cout<<i<<"-->";
+    for(int j=0;j<g.size();j++)
+    cout<<g[i][j]<<" ";
+}
